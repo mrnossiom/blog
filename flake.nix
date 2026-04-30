74056fb2f26ec5c46ed6b47d0a5bd6c4b2ffb24d
Milo Moisson <milo@wiro.world>
2026-04-30 21:47:29 +0100

    add static files from dotfiles for selfhosting

--- flake.nix
+++ flake.nix
@@ -1,10 +1,20 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+
+    dotfiles = {
+      # TODO: wait for alternate server branch to be merged
+      url = "github:mrnossiom/dotfiles?ref=push-uttstrxmxrun";
+      flake = false;
+    };
   };
 
   outputs =
-    { self, nixpkgs }:
+    {
+      self,
+      nixpkgs,
+      dotfiles,
+    }:
     let
       inherit (nixpkgs.lib) genAttrs;
 
@@ -31,7 +41,12 @@
         }
       );
       lpkgs = forAllSystems (system: {
-        inherit (self.packages.${system}) resume-en resume-fr;
+        website-static = pkgs.${system}.callPackage (import "${dotfiles}/pkgs/website-static.nix") { };
+        inherit (self.packages.${system})
+          resume-en
+          resume-fr
+          website-raw
+          ;
       });
     in
     {
@@ -57,17 +72,21 @@
           resume-en = renderTypstPdf "resume-milo_moisson" "private/resume-en.typ";
           resume-fr = renderTypstPdf "cv-milo_moisson" "private/resume-fr.typ";
 
-          website = pkgs.stdenv.mkDerivation {
-            name = "wiro.world-website";
+          website-raw = pkgs.stdenv.mkDerivation {
+            name = "wiro.world-website-raw";
             src = ./.;
             nativeBuildInputs = [ pkgs.zola ];
             buildPhase = "zola build";
-            installPhase = ''
-              cp -r public $out
-              cp ${lpkgs.resume-en} $out/resume-milo_moisson.pdf
-              cp ${lpkgs.resume-fr} $out/cv-milo_moisson.pdf
-            '';
+            installPhase = "cp -r public $out";
           };
+
+          website = pkgs.runCommand "wiro.world-website" { } ''
+            mkdir -p $out
+            cp -rT ${lpkgs.website-raw} $out/
+            cp -rTL ${lpkgs.website-static} $out/
+            cp -rT ${lpkgs.resume-en} $out/resume-milo_moisson.pdf
+            cp -rT ${lpkgs.resume-fr} $out/cv-milo_moisson.pdf
+          '';
 
           inherit (pkgs)
             git-pages-cli
